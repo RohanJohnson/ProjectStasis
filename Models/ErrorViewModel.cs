9ac149711639e90a37336c8c2d1c49566d71acdf
RohanJohnson <rohanjohnson.private@gmail.com>
2024-12-15 20:46:22 +0000

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjectStasis.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ wwwroot/js/App.svelte
@@ -0,0 +1,14 @@
+﻿<svelte:options tag="sv-counter" />
+
+<script>
+	let count = 0;
+</script>
+
+<p>{count}</p>
+<button on:click={() => (count += 1)}>Click</button>
+
+<style lang="scss">
+	p {
+		font-weight: 700;
+	}
+</style>
